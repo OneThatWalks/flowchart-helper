b0ee7c7b31faee378fe4311679c0b2b2ae9b68de
Darrien Singleton <onethatwalks@aol.com>
2019-06-28 11:41:48 -0400

    Ununsed var

--- src/actions/project.tsx
+++ src/actions/project.tsx
@@ -1,4 +1,4 @@
-import { SET_PROJECT_NAME, ProjectActionTypes, ADD_REQUIREMENT, REMOVE_REQUIREMENT, Requirement, AddRequirementAction, RemoveRequirementAction, SetProjectAction } from "../constants/types";
+import { SET_PROJECT_NAME, ADD_REQUIREMENT, REMOVE_REQUIREMENT, Requirement, AddRequirementAction, RemoveRequirementAction, SetProjectAction } from "../constants/types";
 
 export function setProjectName(projectName: string): SetProjectAction {
     return {
