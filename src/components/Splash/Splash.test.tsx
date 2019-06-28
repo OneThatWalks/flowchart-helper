4f86ef62fe8b13371bf78926373c722c5c6ef9af
Darrien Singleton <onethatwalks@aol.com>
2019-06-28 08:32:20 -0400

    splash test

--- src/components/Splash/Splash.test.tsx
+++ src/components/Splash/Splash.test.tsx
@@ -1,3 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { ShallowWrapper, shallow } from 'enzyme';
+import Splash from './Splash';
+
+describe('Splash Component', () => {
+	let wrapper: ShallowWrapper;
+
+	beforeEach(() => {
+		wrapper = shallow(<Splash/>);
+	});
+
+	it('renders without crashing', () => {
+		expect(wrapper).not.toBe(undefined);
+	});
+});
 
