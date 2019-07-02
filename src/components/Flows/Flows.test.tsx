2d37feb7adeeeb1d37eb07c421284fba33ff19b3
Darrien Singleton <onethatwalks@aol.com>
2019-07-02 07:42:27 -0400

    Flows

--- src/components/Flows/Flows.test.tsx
+++ src/components/Flows/Flows.test.tsx
@@ -8,13 +8,20 @@ describe('Flows Component', () => {
 
 	let wrapper: ShallowWrapper;
 	let props: FlowsProps;
-	let historyFn = jest.fn();
+	let historyFn: jest.Mock<any, any>;
 
 	beforeEach(() => {
+		historyFn = jest.fn();
+
 		props = {
 			project: {
 				...testProject,
-				flows: []
+				flows: [
+					{
+						id: 1,
+						name: 'flow-1'
+					}
+				]
 			},
 			history: createBrowserHistory(),
 			location: createLocation(''),
@@ -25,6 +32,7 @@ describe('Flows Component', () => {
 				url: ''
 			}
 		};
+
 
 		props.history.push = historyFn;
 
@@ -54,8 +62,43 @@ describe('Flows Component', () => {
 
 		// Assert
 		expect(historyFn.mock.calls.length).toEqual(1);
+		expect(historyFn.mock.calls[0][0]).toEqual('/flowEditor?id=0');
 	});
 
+	it('displays flows when flows exist in project', () => {
+		// Assert
+		expect(wrapper.find('button.list-group-item').first().text()).toEqual('flow-1')
+	});
 
+	it('redirects to flow editor when flow pressed', () => {
+		// Arrange
+		const button = wrapper.find('button.list-group-item').first();
+
+		// Act
+		button.simulate('click', {
+			currentTarget: {
+				getAttribute: (attribute: string) => { return '1' }
+			}
+		});
+
+		// Assert
+		expect(historyFn.mock.calls.length).toEqual(1);
+		expect(historyFn.mock.calls[0][0]).toEqual('/flowEditor?id=1');
+	});
+
+	it('does not redirect when flow error in redering', () => {
+		// Arrange
+		const button = wrapper.find('button.list-group-item').first();
+
+		// Act
+		button.simulate('click', {
+			currentTarget: {
+				getAttribute: (attribute: string) => { return null }
+			}
+		});
+
+		// Assert
+		expect(historyFn.mock.calls.length).toEqual(0);
+	});
 
 });
--- src/components/Flows/Flows.tsx
+++ src/components/Flows/Flows.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { ProjectState } from 'constants/types';
+import { ProjectState, Flow } from 'constants/types';
 import './Flows.css';
 import { withRouter, RouteComponentProps } from 'react-router';
 
@@ -21,20 +21,43 @@ export interface FlowsState {
 
 export class Flows extends React.Component<FlowsProps, FlowsState> {
 
+	redirectToFlowEditor = (id: number) => {
+		this.props.history.push(`/flowEditor?id=${id}`);
+	}
+
 	onAddButtonClickHandler = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-		this.props.history.push('/flowEditor?id=0');
+		this.redirectToFlowEditor(0);
+	}
+
+	onEditButtonClickHandler = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+		const { redirectToFlowEditor } = this;
+		const flowId = event.currentTarget.getAttribute('data-flowId');
+		if (!flowId || parseInt(flowId, 10) === NaN) {
+			alert('There was an error redirecting to flow.  Please try again.');
+			return;
+		}
+		redirectToFlowEditor(parseInt(flowId as string));
+	}
+
+	mapFlowsToListElements = (item: Flow, index: number) => {
+		const { onEditButtonClickHandler } = this;
+		return (
+			<button key={index} className="list-group-item list-group-item-action" data-flowId={item.id} onClick={onEditButtonClickHandler}>
+				{item.name}
+			</button>
+		);
 	}
 
 	render() {
-		//const flows = this.props.project.flows;
-		const {onAddButtonClickHandler} = this;
+		const { onAddButtonClickHandler, mapFlowsToListElements } = this;
+		const flows = this.props.project.flows;
 		return (
 			<div className="container">
 				<h3>
 					Flows
 				</h3>
 				<ul className="list-group">
-
+					{flows.map(mapFlowsToListElements)}
 				</ul>
 				<button className="btn btn-link" id="addFlowButton" onClick={onAddButtonClickHandler}>Add</button>
 			</div>
